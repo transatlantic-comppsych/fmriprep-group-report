ac9e944e95435e4b7aea1c9ec3c9e7c69556324f
shotgunosine <adenosine@gmail.com>
2021-12-20 16:40:07 -0500

    update expected output

--- fmriprepgr/test/test_reports.py
+++ fmriprepgr/test/test_reports.py
@@ -34,7 +34,7 @@ def test_make_report_snippet():
                        '<script type="text/javascript">',
                        'var subj_qc = {"idx": 0, "chunk": 0, "subject": "22293", "acquisition": "mprage", "reconstruction": "prenorm", "run": 1, "suffix": "dseg", "space": NaN, "desc": NaN, "session": NaN, "task": NaN, "report_type": "dseg", "been_on_screen": false}',
                        '</script>',
-                       '<h2> subject <span class="bids-entity">22293</span>", acquisition <span class="bids-entity">mprage</span>", reconstruction <span class="bids-entity">prenorm</span>", run <span class="bids-entity">1</span>", suffix <span class="bids-entity">dseg</span>"</h2>',
+                       '<h2> subject <span class="bids-entity">22293</span>, acquisition <span class="bids-entity">mprage</span>, reconstruction <span class="bids-entity">prenorm</span>, run <span class="bids-entity">1</span>, suffix <span class="bids-entity">dseg</span></h2>',
                        '<div class="radio">',
                        '<label><input type="radio" name="inlineRadio0" id="inlineRating1" value="1" onclick="qc_update(0, \'report\', this.value)"> Good </label>',
                        '<label><input type="radio" name="inlineRadio0" id="inlineRating0" value="0" onclick="qc_update(0, \'report\', this.value)"> Bad</label>',
