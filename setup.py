a828dd13131757239f19ede8ac915679ca50ef65
shotgunosine <adenosine@gmail.com>
2022-02-17 15:25:02 -0500

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fmriprep-group-report',
-    version='0.0.6',
+    version='0.0.7',
     author='Dylan M. Nielson',
     author_email='dylan.nielson@gmail.com',
     description='Make consolidated group reports from fmriprep output.',
