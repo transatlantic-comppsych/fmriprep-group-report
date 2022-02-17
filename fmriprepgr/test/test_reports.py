e40ff19ded0d0f88b042d0e73ea3e5362b8f91e0
shotgunosine <adenosine@gmail.com>
2022-02-17 15:06:05 -0500

    update test to match field fixes

--- fmriprepgr/test/test_reports.py
+++ fmriprepgr/test/test_reports.py
@@ -33,7 +33,7 @@ def test_make_report_snippet():
            'report_type': 'dseg'}
     expected_output = ['<div id="id-0_filename-sub-22293_acq-mprage_rec-prenorm_run-1_dseg">',
                        '<script type="text/javascript">',
-                       'var subj_qc = {"idx": 0, "chunk": 0, "subject": "22293", "acquisition": "mprage", "reconstruction": "prenorm", "run": 1, "suffix": "dseg", "space": NaN, "desc": NaN, "session": NaN, "task": NaN, "report_type": "dseg", "been_on_screen": false}',
+                       'var subj_qc = {"idx": 0, "chunk": 0, "subject": "22293", "acquisition": "mprage", "reconstruction": "prenorm", "run": 1, "suffix": "dseg", "space": NaN, "desc": NaN, "session": NaN, "task": NaN, "report_type": "dseg", "been_on_screen": false, "rater": NaN, "report": NaN, "note": NaN}',
                        '</script>',
                        '<h2>idx-0: subject <span class="bids-entity">22293</span>, acquisition <span class="bids-entity">mprage</span>, reconstruction <span class="bids-entity">prenorm</span>, run <span class="bids-entity">1</span>, suffix <span class="bids-entity">dseg</span></h2>',
                        '<div class="radio">',
